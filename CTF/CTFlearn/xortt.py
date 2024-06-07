aaec51d1a03fb903dcfd99385be232d44e12b449
CapybarasCTF <movsx@mail.ru>
2024-06-07 07:40:39 +0300

    Добавлены инфо в статью и пытаемся навести порядок с gitignore

--- CTF/CTFlearn/xortt.py
+++ CTF/CTFlearn/xortt.py
@@ -1,5 +1,5 @@
 sotr = "\t\x1b\x11\x00\x16\x0b\x1d\x19\x17\x0b\x05\x1d(\x05\x005\x1b\x1f\t,\r\x00\x18\x1c\x0e"
-pp = open("xoror","w").write(sotr)
+pp = open("Examples/xoror", "w").write(sotr)
 pop = "ctflearn{"
 pip = "\x6a\x6f\x77\x6c\x73"
 qqq = ""
